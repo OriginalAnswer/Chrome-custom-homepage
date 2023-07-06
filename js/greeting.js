62c029c2e53c5ec0aef045e652aefc697817ce36
OriginalAnswer <akiseonair@gmail.com>
2023-07-07 02:56:34 +0900

    .

--- js/greeting.js
+++ js/greeting.js
@@ -21,16 +21,27 @@ function changeGreeting(){
 
 const GREETING_NAME_KEY = "greeting name"
 const savedName = localStorage.getItem(GREETING_NAME_KEY);
-function printName(){
-    greetingShow.innerText = savedName;
-}
+function printName() {
+        greetingShow.innerText = `, ${savedName}`;
+    }
 function addName(e) {
-    // e.preventDefault();
+    e.preventDefault();
     const name = greetingInput.value;
     localStorage.setItem(GREETING_NAME_KEY, name);
+    greetingShow.innerText = `, ${savedName}`;
     printName();
 }
-greetingForm.addEventListener('submit', addName);
+greetingForm.addEventListener('submit', printName);
+
 if (savedName !== null) {
     printName();
-};
+}
+// `
+
+function syncGreeting(){
+    let syncName = greetingInput.value;
+    greetingShow.innerText = `, ${syncName}`;
+    localStorage.setItem(GREETING_NAME_KEY, syncName);
+}
+// 아래 : 띄어쓰기 키 막기
+// onkeypress = "if(event.keyCode == 32) event.returnValue = false;
